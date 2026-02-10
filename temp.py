3240039a7738d483a2a140501c6dc3509fe52953
Megan Leigh <93282848+meganleigh0@users.noreply.github.com>
2026-02-10 08:42:06 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,3 +7,20 @@ DISTINCT(
         SELECTCOLUMNS(Program_Manpower, "ProgramID", Program_Manpower[ProgramID])
     )
 )
+
+PO CTD Color =
+VAR metric = SELECTEDVALUE ( Program_Overview[Metric] )
+VAR v0     = SELECTEDVALUE ( Program_Overview[CTD] )
+VAR v      = ROUND ( v0, 2 )
+RETURN
+SWITCH (
+    TRUE(),
+    ISBLANK(v), BLANK(),
+
+    metric IN { "SPI", "CPI" } && v >= 1.05, "#8EB4E3",
+    metric IN { "SPI", "CPI" } && v >= 0.98, "#339966",
+    metric IN { "SPI", "CPI" } && v >= 0.95, "#FFFF99",
+    metric IN { "SPI", "CPI" },                "#C0504D",
+
+    BLANK()
+)
